257896ff2ad1027f4782c1c9886a1693c189d20f
ruziba3vich <rruzibayevich@gmail.com>
2025-09-14 23:26:37 +0500

    feat: create get user by country request

--- internal/dto/user_dto.go
+++ internal/dto/user_dto.go
@@ -4,4 +4,14 @@ type (
 	CreateUserRequest struct {
 		Username string `json:"username"`
 	}
+
+	GetUsersByCountry struct {
+		PageLimit
+		Country string `form:"country" binding:"required"`
+	}
+
+	PageLimit struct {
+		Page  int `form:"page"  binding:"required,min=1"`
+		Limit int `form:"limit" binding:"required,min=1,max=100"`
+	}
 )
