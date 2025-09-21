0df5187108f6279a3abad6376ff25211c8ed67ba
ruziba3vich <rruzibayevich@gmail.com>
2025-09-22 01:31:13 +0500

    feat: create staging table migration

--- db/migrations/000001_users.up.sql
+++ db/migrations/000001_users.up.sql
@@ -13,6 +13,18 @@ CREATE TABLE IF NOT EXISTS user_data (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS staging_user_data (
+    username TEXT NOT NULL UNIQUE,
+    user_slug TEXT NOT NULL,
+    user_avatar TEXT,
+    country_code CHAR(2),
+    country_name TEXT,
+    real_name TEXT,
+    typename TEXT,
+    total_problems_solved INT NOT NULL DEFAULT 0,
+    total_submissions INT NOT NULL DEFAULT 0
+);
+
 -- keep updated_at fresh automatically
 CREATE OR REPLACE FUNCTION set_updated_at()
 RETURNS TRIGGER AS $$
